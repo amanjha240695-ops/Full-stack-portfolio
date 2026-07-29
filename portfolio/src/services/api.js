88877156e2fd6aeca66c661214f21bffe74c9c2f
amanjha240695-ops <amanjha240695@gmail.com>
2026-07-29 20:21:16 +0530

    api fix for message

--- portfolio/src/services/api.js
+++ portfolio/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://full-stack-portfolio-fzen.onrender.com/",
+  baseURL: "https://full-stack-portfolio-fzen.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
